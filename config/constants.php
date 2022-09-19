7e985dcd517ec38aff153ccb8f2d47681fcef8f3
EricM95 <60743062+EricM95@users.noreply.github.com>
2022-09-19 19:46:03 +0100

    Changed to local db

--- config/constants.php
+++ config/constants.php
@@ -2,11 +2,11 @@
 ob_start();
 session_start();
 
-define('SITEURL', "https://gardenia-order-app.herokuapp.com/");
-define('LOCALHOST', "us-cdbr-east-05.cleardb.net");
-define('DB_USERNAME', "ba7b717cc93067");
-define('DB_PASSWORD', "15f757e3");
-define('DBNAME', "heroku_2a1b9786518a26f");
+define('SITEURL', "http://localhost/Food_Order/");
+define('LOCALHOST', "localhost");
+define('DB_USERNAME', "root");
+define('DB_PASSWORD', "");
+define('DBNAME', "db1_21815369");
 
 $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD, DBNAME) or die(mysqli_error($conn));
 $db_select = mysqli_select_db($conn, DBNAME) or die(mysqli_error($conn));
